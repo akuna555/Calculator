8599185915047becf02451e808d48cf6bc160261
Oksana <oksanakulba35@gmail.com>
2019-02-22 20:39:50 -0600

    write add method

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
            double result = num1 - num2;
            return result;
     }
-        public static double multiply(double num1, double num2){
+        public static double multiply(double num1, double num2) {
                 return num1 * num2;
         }
 
