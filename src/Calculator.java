817d84e6d798244f5b649a908d628aa930025363
akuna555 <43525212+akuna555@users.noreply.github.com>
2019-06-01 11:27:32 -0500

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,7 +13,7 @@ public class Calculator {
         public static double multiply(double num1, double num2) {
                 return num1 * num2;
         }
-        public stati double divide(double num1, double num2){
+        public static double divide(double num1, double num2){
                 return num1/ num2;
         }
 
