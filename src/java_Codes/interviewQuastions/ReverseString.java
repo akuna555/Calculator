f0496c8b73d4a3b76e538d9c009f13b3accf8a90
Oksana <oksanakulba35@gmail.com>
2019-09-25 13:37:43 -0500

    'Hi

---
+++ src/java_Codes/interviewQuastions/ReverseString.java
@@ -0,0 +1,12 @@
+package java_Codes.interviewQuastions;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str ="Oksana";
+        String rev ="";
+
+        for(int i=str.length()-1;i>=0;i--){
+
+        }
+    }
+}
